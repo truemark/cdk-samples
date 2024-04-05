719cd5a34fba5ba1fabbf37d1a569200adc04479
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-04-05 07:08:31 -0600

    Adding initial stack for opensearch-serverless

---
+++ opensearch-serverless/test/opensearch-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OpensearchServerless from '../lib/opensearch-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/opensearch-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OpensearchServerless.OpensearchServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
